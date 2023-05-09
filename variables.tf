127e9f060682ed5abc21cb0909ee67fb3b3ec831
Ganiyat <neeyaholajumoke@gmail.com>
2023-05-09 17:08:00 +0100

    appended vpc-cidr

--- variables.tf
+++ variables.tf
@@ -15,7 +15,9 @@ variable "key" {
 variable "instance-type" {
     default = "t2.small"
 }
-
+variable "vpc-cidr" {
+    default = "10.10.0.0/16"  
+}
 variable "subnet1-cidr" {
     default = "10.0.0.0/24"
   
