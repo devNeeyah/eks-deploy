a4e295b155e1c94b0e539277b1fd16dab5b1ab0f
Ganiyat <neeyaholajumoke@gmail.com>
2023-05-09 17:17:23 +0100

    internet gateway, route table added

--- main.tf
+++ main.tf
@@ -65,4 +65,30 @@ resource "aws_security_group" "dev-vpc-sg" {
   }
 }
 
+resource "aws_internet_gateway" "dev-igw" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "dev-rt" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev-igw.id
+  }
+  tags = {
+    Name = "dev-rt"
+  }
+}
+
+resource "aws_route_table_association" "dev-rt_association" {
+  subnet_id      = aws_subnet.dev_subnet.id 
+
+  route_table_id = aws_route_table.dev-rt.id
+}
+
 
