29ab9215e87b007a8ac77410275e426c54d84769
Ganiyat <neeyaholajumoke@gmail.com>
2023-05-09 14:05:19 +0100

    added subnet/vpc

--- main.tf
+++ main.tf
@@ -20,6 +20,22 @@ resource "aws_instance" "dev-server" {
  key_name = var.key 
  instance_type  = var.instance-type
  associate_public_ip_address = true
+subnet_id = aws_subnet.dev_subnet.id
 
 }
+
+resource "aws_vpc" "dev-vpc" {
+  cidr_block = var.vpc-cidr
+}
+
+resource "aws_subnet" "dev_subnet" {
+  vpc_id     = aws_vpc.dev.vpc.id 
+  cidr_block = var.subnet1-cidr
+  availability_zone = var.subnet_az
+
+  tags = {
+    Name = "dev_subnet"
+  }
+}
+
 
