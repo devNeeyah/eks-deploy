ea4fe76373d66228803e1575f59046fe92eb1c9c
Ganiyat <neeyaholajumoke@gmail.com>
2023-05-09 12:19:50 +0100

    added aws_subnet, ami, key-name

--- variables.tf
+++ variables.tf
@@ -16,12 +16,8 @@ variable "instance-type" {
     default = "t2.small"
 }
 
-variable "vpc-cidr" {
-    default = "10.10.0.0/16"  
-}
-
 variable "subnet1-cidr" {
-    default = "10.10.1.0/24"
+    default = "10.0.0.0/24"
   
 }
 variable "subnet_az" {
